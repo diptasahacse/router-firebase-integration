d787d35d353576168723872007a157f0f6ee4694
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-08 21:58:42 +0600

    turn on react 18 and install firebase, router, bootstrap

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUCwosxqDcy6DITf-1MNnGRYgr1oWwbTY",
+  authDomain: "router-firebase-integrat-ae360.firebaseapp.com",
+  projectId: "router-firebase-integrat-ae360",
+  storageBucket: "router-firebase-integrat-ae360.appspot.com",
+  messagingSenderId: "803260613251",
+  appId: "1:803260613251:web:8f482424c90b680fd1d1b6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
