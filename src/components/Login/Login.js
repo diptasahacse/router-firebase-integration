50195ef45e36e7df0d73f515ef9c12ce52cffe67
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-04-10 02:04:27 +0600

    use Path from location to redirect the pages which tried to visit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,11 +3,23 @@ import { Button, Form } from 'react-bootstrap';
 import useFirebase from '../../hooks/useFirebase';
 import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
 import auth from '../../firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    // const {signInwithGoogleHandler} = useFirebase();
+    let location = useLocation();
+    let navigate = useNavigate();
+    let from = location.state.from.pathname || "/";
 
     const [signInWithGoogle, user,error] = useSignInWithGoogle(auth);
+
+    // signInWithGoogle()
+
+    const signInWithGoogleHandler = () =>{
+        signInWithGoogle()
+        .then(()=>{
+            navigate(from, { replace: true });
+        })
+    }
     
     return (
         <div className='container'>
@@ -15,7 +27,7 @@ const Login = () => {
                 <h3>Login</h3>
             </div>
             <div className='text-center'>
-                <button onClick={()=>signInWithGoogle()} className='btn btn-danger'>Sign in with Google</button>
+                <button onClick={signInWithGoogleHandler} className='btn btn-danger'>Sign in with Google</button>
             </div>
             <div className='w-75 mx-auto'>
                 <Form>
